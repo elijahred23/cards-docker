c1b98b28a655c15cf80dbf80553becf7253a127c
elijahred23 <elijahred23@gmail.com>
2023-10-20 02:40:46 -0500

    Eli-6: Added royal and straight flush

--- poker/HandRankings.ts
+++ poker/HandRankings.ts
@@ -4,12 +4,74 @@ class HandRankings{
     constructor(){
 
     }
-    isRoyalFlush(hand: Card[]): boolean{
-        return true;
+    isRoyalFlush(hand: Card[]): boolean {
+        const suitCards: { [suit: number]: Card[] } = {
+          1: [],
+          2: [],
+          3: [],
+          4: [],
+        };
+      
+        for (const card of hand) {
+          if (card.value >= 10) {
+            suitCards[card.suit].push(card);
+          }
+        }
+      
+        if (![1, 2, 3, 4].some((suit) => suitCards[suit].length === 5)) {
+          return false;
+        }
+      
+        for (let suit = 1; suit <= 4; suit++) {
+          if (suitCards[suit].length !== 5) {
+            continue;
+          }
+      
+          const sortedCards = suitCards[suit].sort((a, b) => a.value - b.value);
+      
+          if (sortedCards[0].value === 10 && sortedCards[4].value === 14) {
+            return true; // Found a royal flush!
+          }
+        }
+      
+        return false;
+      }
+      
+      isStraightFlush(hand: Card[]): boolean {
+        const suitCards: { [suit: number]: Card[] } = {
+          1: [],
+          2: [],
+          3: [],
+          4: [],
+        };
+      
+        for (const card of hand) {
+            suitCards[card.suit].push(card);
+        }
+      
+        // Find the suit with at least 5 cards
+        const straightFlushSuit = [1, 2, 3, 4].find((suit) => suitCards[suit].length >= 5);
+      
+        if (!straightFlushSuit) {
+          return false; // No suit with 5 or more cards, no straight flush
+        }
+      
+        const sortedCards = suitCards[straightFlushSuit].sort((a, b) => a.value - b.value);
+      
+        for (let i = 0; i < sortedCards.length - 4; i++) {
+          if (
+            sortedCards[i].value === sortedCards[i + 1].value - 1 &&
+            sortedCards[i + 1].value === sortedCards[i + 2].value - 1 &&
+            sortedCards[i + 2].value === sortedCards[i + 3].value - 1 &&
+            sortedCards[i + 3].value === sortedCards[i + 4].value - 1
+          ) {
+            return true; // Found a straight flush!
+          }
+        }
+      
+        return false;
     }
-    isStraightFlush(hand:Card[]){
-
-    }
+      
     isFourOfAKind(hand:Card[]){
 
     }
