c3db99eecfacdad4925cfc30f786bdcae0997385
elijahred23 <elijahred23@gmail.com>
2023-10-19 21:43:24 -0500

    Eli-2: Finished deck and starting hand rankings

---
+++ poker/HandRankings.ts
@@ -0,0 +1,36 @@
+import Card from '../common/Card';
+
+class HandRankings{
+    constructor(){
+
+    }
+    isRoyalFlush(hand:Card[]){
+
+    }
+    isStraightFlush(hand:Card[]){
+
+    }
+    isFourOfAKind(hand:Card[]){
+
+    }
+    isFullHouse(hand:Card[]){
+
+    }
+    isFlush(hand:Card[]){
+
+    }
+    isStraight(hand:Card[]){
+
+    }
+    isThreeOfAKind(hand:Card[]){
+
+    }
+    isTwoPair(hand:Card[]){
+
+    }
+    isHighCard(hand:Card[]){
+        
+    }
+}
+
+export default HandRankings;
