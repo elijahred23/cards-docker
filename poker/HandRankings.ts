36bc416d0d4429cc19c47c94688548544b28c0a0
elijahred23 <elijahred23@gmail.com>
2023-10-22 23:48:55 -0500

    Eli-9: Added full house method

--- poker/HandRankings.ts
+++ poker/HandRankings.ts
@@ -1,112 +1,139 @@
 import Card from '../common/Card';
 
-class HandRankings{
-    constructor(){
+class HandRankings {
+    constructor() {
 
     }
     isRoyalFlush(hand: Card[]): boolean {
         const suitCards: { [suit: number]: Card[] } = {
-          1: [],
-          2: [],
-          3: [],
-          4: [],
+            1: [],
+            2: [],
+            3: [],
+            4: [],
         };
-      
+
         for (const card of hand) {
-          if (card.value >= 10) {
-            suitCards[card.suit].push(card);
-          }
+            if (card.value >= 10) {
+                suitCards[card.suit].push(card);
+            }
         }
-      
+
         if (![1, 2, 3, 4].some((suit) => suitCards[suit].length === 5)) {
-          return false;
+            return false;
         }
-      
+
         for (let suit = 1; suit <= 4; suit++) {
-          if (suitCards[suit].length !== 5) {
-            continue;
-          }
-      
-          const sortedCards = suitCards[suit].sort((a, b) => a.value - b.value);
-      
-          if (sortedCards[0].value === 10 && sortedCards[4].value === 14) {
-            return true; // Found a royal flush!
-          }
+            if (suitCards[suit].length !== 5) {
+                continue;
+            }
+
+            const sortedCards = suitCards[suit].sort((a, b) => a.value - b.value);
+
+            if (sortedCards[0].value === 10 && sortedCards[4].value === 14) {
+                return true; // Found a royal flush!
+            }
         }
-      
+
         return false;
-      }
-      
-      isStraightFlush(hand: Card[]): boolean {
+    }
+
+    isStraightFlush(hand: Card[]): boolean {
         const suitCards: { [suit: number]: Card[] } = {
-          1: [],
-          2: [],
-          3: [],
-          4: [],
+            1: [],
+            2: [],
+            3: [],
+            4: [],
         };
-      
+
         for (const card of hand) {
             suitCards[card.suit].push(card);
         }
-      
+
         // Find the suit with at least 5 cards
         const straightFlushSuit = [1, 2, 3, 4].find((suit) => suitCards[suit].length >= 5);
-      
+
         if (!straightFlushSuit) {
-          return false; // No suit with 5 or more cards, no straight flush
+            return false; // No suit with 5 or more cards, no straight flush
         }
-      
+
         const sortedCards = suitCards[straightFlushSuit].sort((a, b) => a.value - b.value);
-      
+
         for (let i = 0; i < sortedCards.length - 4; i++) {
-          if (
-            sortedCards[i].value === sortedCards[i + 1].value - 1 &&
-            sortedCards[i + 1].value === sortedCards[i + 2].value - 1 &&
-            sortedCards[i + 2].value === sortedCards[i + 3].value - 1 &&
-            sortedCards[i + 3].value === sortedCards[i + 4].value - 1
-          ) {
-            return true; // Found a straight flush!
-          }
+            if (
+                sortedCards[i].value === sortedCards[i + 1].value - 1 &&
+                sortedCards[i + 1].value === sortedCards[i + 2].value - 1 &&
+                sortedCards[i + 2].value === sortedCards[i + 3].value - 1 &&
+                sortedCards[i + 3].value === sortedCards[i + 4].value - 1
+            ) {
+                return true; // Found a straight flush!
+            }
         }
-      
+
         return false;
     }
-    createCardMap(hand:Card[]):Map<number, Card[]>{
-      let cardMap = new Map();
-      for(let card of hand){
-        if(cardMap.has(card.value)){
-          cardMap.get(card.value).push(card);
-        } else {
-          cardMap.set(card.value, [card])
+    createCardMap(hand: Card[]): Map<number, Card[]> {
+        let cardMap = new Map();
+        for (let card of hand) {
+            if (cardMap.has(card.value)) {
+                cardMap.get(card.value).push(card);
+            } else {
+                cardMap.set(card.value, [card])
+            }
         }
-      }
-      return cardMap;
+        return cardMap;
     }
-    isFourOfAKind(hand:Card[]):boolean {
-      let cardMap = this.createCardMap(hand);
+    isFourOfAKind(hand: Card[]): boolean {
+        let cardMap = this.createCardMap(hand);
 
-      for (let value = 2; value <= 14; value++) {
-        const cardArray = cardMap.get(value);
-        if (cardArray !== undefined && cardArray.length > 3) {
-          return true;
+        for (let value = 2; value <= 14; value++) {
+            const cardArray = cardMap.get(value);
+            if (cardArray !== undefined && cardArray.length > 3) {
+                return true;
+            }
         }
-      }
-      return false;
+        return false;
     }
-    isFullHouse(hand:Card[]){
-      let cardMap = this.createCardMap(hand);
-      
+    isFullHouse(hand: Card[]) {
+        const cardMap = this.createCardMap(hand);
+        let hasThreeOfKind = false;
+        let hasPair = false;
+
+        for (let value = 2; value <= 14; value++) {
+            if (cardMap.has(value)) {
+                const cards = cardMap.get(value);
+                if (cards == undefined) {
+                    continue;
+                }
+                else if (cards.length === 3) {
+                    if (hasThreeOfKind == true) {
+                        hasPair = true;
+                    } else {
+                        hasThreeOfKind = true;
+                    }
+                } else if (cards.length === 2) {
+                    hasPair = true;
+                }
+            }
+
+            // If you found both a Three of a Kind and a Pair, it's a Full House
+            if (hasThreeOfKind && hasPair) {
+                return true;
+            }
+        }
+
+        return false;
     }
-    isFlush(hand:Card[]){
+
+    isFlush(hand: Card[]) {
 
     }
-    isStraight(hand:Card[]){
+    isStraight(hand: Card[]) {
 
     }
-    isThreeOfAKind(hand:Card[]){
+    isThreeOfAKind(hand: Card[]) {
 
     }
-    isTwoPair(hand:Card[]){
+    isTwoPair(hand: Card[]) {
 
     }
 }
